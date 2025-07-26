39ec58d3baeaaacb47a8b3aa9b482a0c2c4714cd
Anshu <anshuy115@gmail.com>
2025-07-26 21:01:31 +0530

    multi ingradien list fix

--- components/IngredientMultiSelect.js
+++ components/IngredientMultiSelect.js
@@ -42,8 +42,10 @@ const handleSearch = () => {
   setIsDisabled(true);
   setTimeout(() => setIsDisabled(false), 5000);
 };
+
+
   return (
-    <div className="mx-auto p-4 w-full">
+    <div className="mx-auto  w-full">
       <div className="mb-4 relative">
         <label className="block mb-1 font-medium">Ingredients</label>
         <input
@@ -54,24 +56,25 @@ const handleSearch = () => {
           placeholder="Type to search..."
         />
 
-        {debouncedQuery && (
-          <ul className="absolute z-10 w-full border border-t-0 border-gray-300 max-h-48 overflow-auto rounded-b-md bg-gray-100 text-black shadow-md">
-            {filteredIngredients.length > 0 ? (
-              filteredIngredients.map((item) => (
-                <li
-                  key={item.id}
-                  onClick={() => handleSelect(item)}
-                  className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
-                >
-                  {item.name}
-                  <span className="text-sm text-gray-400"> ({item.category})</span>
-                </li>
-              ))
-            ) : (
-              <li className="px-4 py-2 text-gray-400">No results</li>
-            )}
-          </ul>
-        )}
+       {debouncedQuery && (
+  <div className="absolute z-10 w-full border border-t-0 border-gray-300 max-h-48 overflow-auto rounded-b-md bg-gray-100 text-black shadow-md">
+    {filteredIngredients.length > 0 ? (
+      filteredIngredients.map((item) => (
+        <div
+          key={item.id}
+          onClick={() => handleSelect(item)}
+          className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+        >
+          {item.name}
+          <span className="text-sm text-gray-400"> ({item.category})</span>
+        </div>
+      ))
+    ) : (
+      <div className="px-4 py-2 text-gray-400">No results</div>
+    )}
+  </div>
+)}
+
       </div>
 
       {selected.length > 0 && (
